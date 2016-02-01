18738b16c41c53954a3ac8208beffc610ff8c9b8
Steve Conger <spconger@gmail.com>
2016-02-01 10:56:28 -0800

    CommitServiceExample212016

---
+++ App_Code/AuthorBookService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AuthorBookService" in code, svc and config file together.
+public class AuthorBookService : IAuthorBookService
+{
+
+    BookReviewDbEntities br = new BookReviewDbEntities();
+
+    public List<string> GetAuthors()
+    {
+        var auths = from a in br.Authors
+                    
+                    orderby a.AuthorName
+                    select new { a.AuthorName};
+        
+        List<string> authors = new List<string>();
+        foreach(var au in auths)
+        {
+            authors.Add(au.AuthorName.ToString());
+        }
+
+        return authors;
+    }
+
+    public List<BookLite> GetBooks(string author)
+    {
+        var bks = from b in br.Books
+                  from a in b.Authors
+                  where a.AuthorName.Equals(author)
+                  select new { b.BookTitle, b.BookISBN, b.BookEntryDate };
+
+        List<BookLite> books = new List<BookLite>();
+
+        foreach (var b in bks)
+        {
+            BookLite blite = new BookLite();
+            blite.Title = b.BookTitle;
+            blite.ISBN = b.BookISBN;
+            blite.EntryDate = b.BookEntryDate.ToShortDateString();
+
+            books.Add(blite);
+        }
+
+        return books;
+    }
+
+
+   
+}
---
+++ App_Code/IAuthorBookService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAuthorBookService" in both code and config file together.
+[ServiceContract]
+public interface IAuthorBookService
+{
+    [OperationContract]
+    List<string> GetAuthors();
+    [OperationContract]
+    List<BookLite> GetBooks(string author);
+   
+}
+
+[DataContract]
+public class BookLite
+{
+    [DataMember]
+    public string Title { set; get; }
+
+   
+    [DataMember]
+    public string ISBN { set; get; }
+
+    [DataMember]
+    public string EntryDate { set; get; }
+}
